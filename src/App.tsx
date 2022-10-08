a55e21a756fabceb0fd8d3428af7ac486d64ea27
Damola Olutoke <olutokeezekiel18@gmail.com>
2022-10-08 22:02:24 +0100

    Add User page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 // import './App.css';
 import "./css/styles/App.scss"
 import Login from './pages/Login/Login';
-import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/SidebarDetails/Sidebar';
+import User from './pages/User/User';
 function App() {
 
   return (
     <>
       {/* <Login/> */}
-      <Navbar/>
-      <Sidebar/>
+      <User/>
     </>
   );
 }
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,8 +1,64 @@
 import React from 'react'
 
+// components imports
+import Navbar from '../../components/Navbar/Navbar'
+import Sidebar from '../../components/SidebarDetails/Sidebar'
+import TableData from '../../components/UserDataTable/TableData'
+
+// style imports
+import "./user.scss"
+const userDataContent = [ 
+  {
+    img:"https://res.cloudinary.com/dutcp8qkx/image/upload/v1665219478/Lendsqr/icons/users_ly86vq.svg",
+    title:"USERS",
+    number:"2,543"
+  },
+  {
+    img:"https://res.cloudinary.com/dutcp8qkx/image/upload/v1665219477/Lendsqr/icons/active_users_d6jvvw.svg",
+    title:"ACTIVE USERS",
+    number:"2,543",
+  },
+  {
+    img:"https://res.cloudinary.com/dutcp8qkx/image/upload/v1665219475/Lendsqr/icons/user_loans_dkfytg.svg",
+    title:"USERS WITH LOANS",
+    number:"12,453"
+  },
+  {
+    img:"https://res.cloudinary.com/dutcp8qkx/image/upload/v1665219480/Lendsqr/icons/user_savings_p4in7p.svg",
+    title:"USERS WITH SAVINGS",
+    number:"102,453"
+  },
+  ]
+
+const UserDataContainer = () => {
+  return (
+    <div className="userheader">
+      { userDataContent.map( user => (
+        <div className='card' key={user.img}>
+          <img src={user.img} alt="user_icon"/>
+          <p className='text'>{user.title}</p>
+          <p className='num'>{user.number}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+
 const User = () => {
   return (
-    <div>User</div>
+    <>
+        <Navbar/>
+        <Sidebar/>
+        <div className="userContainer">
+            <h1>Users</h1>
+
+            <UserDataContainer/>
+
+            <TableData/>
+        </div>
+    </>
+    
   )
 }
 
